c2caa80763ca53f82afa000908f02bdcc9ffc753
Txxxr <viktor.shmanin@yandex.ru>
2022-03-06 01:16:37 +0300

    Добавил все во вьюшку

--- Podfile
+++ Podfile
@@ -1,7 +1,20 @@
-platform :ios, "10.0"
-use_frameworks!
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
 target 'Weather_VIPER' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
 
+  # Pods for Weather_VIPER
+pod 'SnapKit'
+pod 'RealmSwift'
+  target 'Weather_VIPERTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
 
+  target 'Weather_VIPERUITests' do
+    # Pods for testing
+  end
 
 end
--- Weather_VIPER/Classes/Assemblys/Modules/Main/View/MainViewController.swift
+++ Weather_VIPER/Classes/Assemblys/Modules/Main/View/MainViewController.swift
@@ -7,13 +7,214 @@
 //
 
 import UIKit
+import SnapKit
+import RealmSwift
+import Foundation
+import CoreLocation
+import Network
+
+struct Local_Names: Codable{
+    var ru: String = ""
+    
+}
+
+struct CorrectCity: Codable{
+    var name: String = ""
+    var local_names: Local_Names = Local_Names()
+}
+
+
+struct Weather: Codable {
+    var id: Int
+    var main: String
+    var description: String
+    var icon: String
+}
+struct Main: Codable {
+    var temp: Double = 0.0
+    var pressure: Int = 0
+    var humidity: Int = 0
+    
+}
+struct WeatherData: Codable{
+    var weather: [Weather] = []
+    var main: Main = Main()
+    var name: String = ""
+}
+
+
+
+
+
 
 class MainViewController: UIViewController, MainViewInput {
     var output: MainViewOutput!
     
+    var backgroundImage = UIImageView()
+    var cityName = UILabel()
+    var weatherIcon = UIImageView()
+    var temperatureLabel = UILabel()
+    var weatherDescription = UILabel()
+    
+    
+    let locationManager = CLLocationManager()
+    var weatherData = WeatherData()
+    var correctCity = CorrectCity()
+    var rightData: [CorrectCity] = []
+    //let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    let monitor = NWPathMonitor()
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        output.viewDidLoad()
+        //output.viewDidLoad()
+        initialize()
+        startLocationManager()
+    }
+    
+    
+    func getData(){
+//        guard let data = realm.objects(SavedData.self).last else {return}
+//        cityName.text = data.city
+//        iconName.image = UIImage(named: data.icon)
+//        tempLabel.text = data.temp
+//        weatherDescription.text = data.descr
+//        print(data.city)
+//        print(data.temp)
+//        print(data.descr)
+    }
+    
+    func setData(){
+       
+//        try! realm.write{
+//            //models.icon = weatherData.weather[0].icon
+//            models.temp = tempLabel.text!
+//            models.city = cityName.text!
+//            models.descr = weatherDescription.text!
+//            realm.add(models)
+//            //print(models.city)
+//        }
+//        print("Data setting")
+//        print(models.icon)
+//        print(models.temp)
+//        print(models.city)
+//        print(models.descr)
+    }
+    
+    
+    func startLocationManager(){
+        locationManager.requestWhenInUseAuthorization()
+        print("requested")
+        if CLLocationManager.locationServicesEnabled(){
+            print("enabled")
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyThreeKilometers
+            locationManager.pausesLocationUpdatesAutomatically = true
+            locationManager.startUpdatingLocation()
+            
+        }
+    }
+
+    func getWeatherInfo(latitude: Double, longtitude: Double){
+        let session = URLSession.shared
+        let url = URL(string: "https://api.openweathermap.org/data/2.5/weather?lat=\(latitude.description)&lon=\(longtitude.description)&appid=7ecd06499c4370fe484ef26aff81cc5c&units=metric&lang=ru")!
+        let request = session.dataTask(with: url) { (data, response, error) in
+        guard error == nil else {
+            print("Data task error: \(error!.localizedDescription)")
+            return
+        }
+            do {
+                self.weatherData = try JSONDecoder().decode(WeatherData.self, from: data!)
+                DispatchQueue.main.async {
+                    self.correctCity(latitude: latitude, longtitude: longtitude)
+                    self.updateView()
+                }
+            } catch  {
+                print(error)
+            }
+        }
+        request.resume()
+    }
+    func updateView(){
+        weatherDescription.text = DataSource.weatherIDs[weatherData.weather[0].id]
+        temperatureLabel.text = Int(round(weatherData.main.temp)).description + "º"
+        weatherIcon.image = UIImage(named: weatherData.weather[0].icon)
+        //setData()
+        
+}
+    func rightCity(){
+        cityName.text = rightData[0].local_names.ru
+    }
+    func correctCity(latitude: Double, longtitude: Double){
+        let session = URLSession.shared
+        let url = URL(string: "http://api.openweathermap.org/geo/1.0/reverse?lat=\(latitude)&lon=\(longtitude)&limit=10&appid=7ecd06499c4370fe484ef26aff81cc5c")!
+        let request = session.dataTask(with: url) { (data, response, error) in
+        guard error == nil else {
+            print("Data task error: \(error!.localizedDescription)")
+            return
+        }
+            do {
+                self.rightData = try JSONDecoder().decode([CorrectCity].self, from: data!)
+                DispatchQueue.main.async {
+                    self.rightCity()
+                }
+            } catch  {
+                print(error)
+            }
+        }
+        request.resume()
+    }
+    
+    
+    
+    
+    
+    
+    func initialize() {
+        backgroundImage.image = UIImage(named: "backgroundImage")
+        view.addSubview(backgroundImage)
+        backgroundImage.snp.makeConstraints{maker in
+            maker.top.equalToSuperview().inset(0)
+            maker.bottom.equalToSuperview().inset(0)
+            maker.left.equalToSuperview().inset(0)
+            maker.right.equalToSuperview().inset(0)
+        }
+
+        
+        cityName.text = "Your city"
+        cityName.font = UIFont(name: "Georgia-Bold", size: 25)
+        view.addSubview(cityName)
+        cityName.snp.makeConstraints{maker in
+            maker.centerX.equalToSuperview()
+            maker.top.equalToSuperview().inset(125)
+        }
+        
+        weatherIcon.image = UIImage(named: "weatherIcon")
+        view.addSubview(weatherIcon)
+        weatherIcon.snp.makeConstraints{maker in
+            maker.width.equalTo(50)
+            maker.height.equalTo(50)
+            maker.centerX.equalToSuperview()
+            maker.top.equalTo(cityName).inset(50)
+            
+        }
+        
+        temperatureLabel.text = " ºC"
+        temperatureLabel.font = UIFont(name: "Georgia-Bold", size: 25)
+        view.addSubview(temperatureLabel)
+        temperatureLabel.snp.makeConstraints{maker in
+            maker.centerX.equalToSuperview()
+            maker.top.equalTo(weatherIcon).inset(70)
+        }
+        
+        weatherDescription.text = "Weather desription"
+        weatherDescription.font = UIFont(name: "Georgia-Bold", size: 20)
+        view.addSubview(weatherDescription)
+        weatherDescription.snp.makeConstraints{maker in
+            maker.top.equalTo(temperatureLabel).inset(50)
+            maker.centerX.equalToSuperview()
+        }
     }
 }
 
@@ -31,5 +232,21 @@ extension MainViewController {
 
 // MARK: Button Action
 extension MainViewController {
+    
+}
+
+
+
+extension MainViewController: CLLocationManagerDelegate{
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let curentLocation = locations.last {
+            print (curentLocation.coordinate.latitude, curentLocation.coordinate.longitude)
+            getWeatherInfo(latitude: curentLocation.coordinate.latitude, longtitude: curentLocation.coordinate.longitude)
+            //setData()
+        }
+        
+        
+    }
     
 }
